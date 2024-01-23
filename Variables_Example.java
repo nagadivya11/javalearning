076623a5ae1cefe967c1a259016d244fd8cb611a
nagadivya11 <152214332+nagadivya11@users.noreply.github.com>
2024-01-22 21:06:53 -0500

    assignment1

---
+++ Variables_Example.java
@@ -0,0 +1,14 @@
+public class Variables_Example {
+	
+	public static void main(String args[]) {
+		
+		int number1=12;
+		int number2=13;
+		System.out.println("number1 and number2 values are not equal");
+		
+		int number3=100;
+		int number4=100;
+		System.out.println("number3 and number4 values are  equal ");
+		
+	}
+}
---
+++ Variables_Example1.java
@@ -0,0 +1,22 @@
+public class Variables_Example1 {
+
+ public static void main (String args[]){
+  
+  int number1 = 12;
+  int number2 = 13;
+  int number3 =11 ;
+  
+  int sum = number1 + number2 + number3;
+  System.out.println("Sum of three numbers is :" + sum);
+  
+  int average = (number1 + number2 + number3) / 3;
+  System.out.println("avergae of three numbers is :" + average);
+  
+  int sub = number1-number2-number3;
+  System.out.println("sub of three numbers is :" + sub);
+  
+  int mul = number1*number2*number3;
+  System.out.println("mul of three numbers is :"+ mul);
+  
+ }
+}
